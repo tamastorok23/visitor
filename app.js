54bbe04c9616ce86355c9c53c596b56b8e7b37a5
Tamas Torok <tamastorok23@gmail.com>
2024-09-20 16:15:02 +0200

    aaa

--- app.js
+++ app.js
@@ -48,6 +48,9 @@ async function visitPage(url) {
     });
 
     const page = await browser.newPage();
+	
+	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+	await page.setViewport({ width: 1280, height: 800 });
 
     try {
         console.log('Navigating to page...');
