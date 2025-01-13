f501c0b172ee46d0b86bbd482aac2f26fbf964ae
Tamas Torok <tamastorok23@gmail.com>
2025-01-13 22:43:05 +0100

    aaa

--- app.js
+++ app.js
@@ -52,12 +52,13 @@ async function visitPage(url) {
 
     const page = await browser.newPage();
 	
+	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+	
 	await page.setExtraHTTPHeaders({
 		'Accept-Language': 'en-US,en;q=0.9',
 		'Cache-Control': 'no-cache',
 	});
 	
-	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 	await page.setViewport({ width: 1280, height: 800 });
 
     try {
